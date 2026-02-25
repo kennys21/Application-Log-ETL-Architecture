6fc1230157af074f013b971c1ce75b1e418e24af
kennys <riffatsaya30@gmail.com>
2026-02-25 19:31:53 +0700

    Update and rename log-processor.py to Scripts/log-processor.py

--- Scripts/log-processor.py
+++ Scripts/log-processor.py
@@ -1,6 +1,8 @@
+#Importing necessary libraries
 import re
 import csv
 
+#this is the pattern for cleaning the log lines cause we will use regex to split and clean it
 pattern = re.compile(r"""
     (?P<timestamp>[\d\-:\s,]+)
     \s-\s
@@ -11,10 +13,12 @@ pattern = re.compile(r"""
     \-status=(?P<status>\w+)
 """, re.VERBOSE)
 
+#making the new csv file and write the header
 with open("structured_logs.csv", "w", newline="") as csvfile:
     writer = csv.writer(csvfile)
-    writer.writerow(["timestamp", "level", "user_id", "action", "status"])
-
+    writer.writerow(["timestamp",
+                     "level", "user_id", "action", "status"])
+    #open the log file and read it line by line, then apply the regex pattern to extract the relevant information and write it to the csv file
     with open("random_app.log", "r") as f:
         for line in f:
             match = pattern.search(line)
